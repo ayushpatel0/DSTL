1757c5ea2b84eb5df33fbc0a8bf54951c8c2bf15
ayushpatel0 <90753146+ayushpatel0@users.noreply.github.com>
2022-11-09 18:16:33 +0530

    Add files via upload

---
+++ Difference.C
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a[10],b[10],c[10],d[10],i,j,k=0,m=0,l,n1,n2;
+    clrscr();
+    // input for the set A
+    printf("Enter the number of element in set A\n");
+    scanf("%d",&n1);
+    printf("Enter the elements \n");
+    for(i = 0; i < n1; i++)
+	scanf("%d",&a[i]);
+    // input for the set B
+    printf("Enter the number of element in set B \n");
+    scanf("%d",&n2);
+    printf("Enter the elements \n");
+    for(i = 0; i < n2; i++)
+	scanf("%d",&b[i]);
+    // calculation for A-B
+    for(i = 0; i < n1; i++)
+    {
+	for(j = 0; j < n2; j++)
+	{
+	    if(b[j]==a[i])
+	     break;
+	}
+	if(j==n2)
+	{
+	    //
+	    for(l = 0; l < k; l++)
+	    {
+	    if(c[l]==a[i])
+	     break;
+	    }
+	    if(l==k)
+	    {
+	    c[k]=a[i];
+	    k++;
+	    }
+	}
+    }
+    // calculation for B-A
+    for(i = 0; i < n2; i++)
+    {
+	for(j = 0;j < n1; j++)
+	{
+	    if(b[i]==a[j])
+	     break;
+	}
+	if(j==n1)
+	{
+	    for(l = 0; l < m; l++)
+	    {
+		if(d[l]==b[i])
+		 break;
+	    }
+	    if(l==m)
+	    {
+		d[m]=b[i];
+		m++;
+	    }
+	}
+    }
+    // printing the final results
+    printf("Difference of A-B is:-\n");
+    for(i = 0; i < k; i++)
+	printf("%d ",c[i]);
+    printf("\n");
+    printf("Difference of B-A is:-\n");
+    for(i = 0; i < m; i++)
+	printf("%d ",d[i]);
+    getch();
+}
